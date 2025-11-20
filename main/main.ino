d33dc45e2c39569fc3e619c947f763df1e1f316a
Pedro N <itiropedro@gmail.com>
2025-11-20 15:52:48 -0300

    Update main.ino and main.ino

--- main/main.ino
+++ main/main.ino
@@ -8,12 +8,18 @@
 #define ROTARY_ENC_PCINT_B PCINT23
 #define ROTARY_ENC_PCINT_AB_IE PCIE2
 
+#define MAX_STRENGTH 200
+
 #define POS_SENSOR PB2 // switch (absolute position)
 #define GP_BUTTON PB0 // general purpose button
+#define SERVO PB1
 
 #include "Rotary.h"
 volatile long count = 0; // encoder_rotativo = posicao relativa depois de ligado
 volatile bool absolute_sw = false; // chave de posicao do volante ativa?
+volatile long centro = 0;
+bool centralizado = 0;
+int x = 0;
 
 Rotary r = Rotary(ROTARY_ENC_A, ROTARY_ENC_B);
 
@@ -34,7 +40,8 @@ void setup() {
   initPWM();
   sei();
 
-  Idle();
+  idle();
+  encontrarCentro();
 }
 
 void initPWM() {
@@ -45,23 +52,21 @@ void initPWM() {
 }
 
 void setPWM(unsigned char val) {
-  OCR2A = val<200?val:200;
+  OCR2A = val < MAX_STRENGTH ? val: MAX_STRENGTH;
 }
 
-void Stop() {
-  PORTB |= (1<<ATUA_CW);
-  PORTB |= (1<<ATUA_CCW);
+void stop() {
+  PORTB |= (1<<ATUA_CW) | (1<<ATUA_CCW);
 }
 
-void Idle() {
+void idle() {
   setPWM(0);
-  PORTB &= ~(1<<ATUA_CW);
-  PORTB &= ~(1<<ATUA_CCW);
+  PORTB &= ~((1<<ATUA_CW) | (1<<ATUA_CCW));
 }
 
-void Move(unsigned char power, bool cw = true) {
+void move(unsigned char power, bool cw = true) {
   if (power == 0)
-    Idle();
+    idle();
   else {
     if (cw) {
       PORTB |= (1<<ATUA_CW);
@@ -74,16 +79,47 @@ void Move(unsigned char power, bool cw = true) {
   }
 }
 
-int dir = ATUA_CCW;
+void encontrarCentro(){
+  unsigned long tempoParaFrenagem = 0;
+  absolute_sw = 0==(PINB&(1<<POS_SENSOR));
+  // se o volante começar em cima da chave
+  while(absolute_sw == 0) move(170, 0);
+
+  stop();
+  // enquanto nao estiver na chave, roda no sentido horario
+  while(absolute_sw != 0) move(160, 1);
+  stop();
+  tempoParaFrenagem = millis();
+  count = 0;
+  while(millis()-tempoParaFrenagem < 1000) centro = 730-count;
+  count = 0;
+}
+
+void centralizarVolante(){
+
+  while(count < centro-5){ 
+    move(155, 1);
+  }
+  stop();
+  while(count > centro+5) {
+    move(155, 0);
+  }
+  stop();
+}
 
 void loop() {
-  Move(100, false); // move ccw
-  Move(100); // move cw
+  centralizarVolante();
+
+  // move(100, false); // move ccw
+  // move(100); // move cw
   // debug only info
   if (millis()%300==0) {
     Serial.print(count);
     Serial.print(", ");
     Serial.println(absolute_sw==true?'1':'0');
+    // Serial.print(", ");
+    // Serial.println(distanciaRestante);
+    Serial.println(centralizado);
   }
 }
 
@@ -92,12 +128,8 @@ ISR(PCINT2_vect) {
   if (result == DIR_NONE) {
     // do nothing
   }
-  else if (result == DIR_CW) {
-    count--;
-  }
-  else if (result == DIR_CCW) {
-    count++;
-  }
+  else if (result == DIR_CW) count--;
+  else if (result == DIR_CCW) count++;
 
   absolute_sw = 0==(PINB&(1<<POS_SENSOR));
 }
