97e94bf80831f573bfb565889da09314b12aa081
Pedro N <itiropedro@gmail.com>
2025-12-04 16:55:11 -0300

    comentários

--- main/main.ino
+++ main/main.ino
@@ -8,23 +8,41 @@
 #define ROTARY_ENC_PCINT_B PCINT23
 #define ROTARY_ENC_PCINT_AB_IE PCIE2
 
-#define VOLTAS_ENCODER_DA_CHAVE_PARA_CENTRO 730
 #define ATUA_CW PB4
 #define ATUA_CCW PB5 // HIGH aqui gira CCW
 #define ATUA_STRENGTH PB3
 
-#define MAX_STRENGTH 160
-#define OFFSET 5
 #define POS_SENSOR PB2 // switch (absolute position)
 #define GP_BUTTON PB0 // general purpose button
 #define SERVO PB1
 #define DEBOUNCE 125
 
+/*
+ * Valores testados com base no volante fornecido
+ */
+
+#define VOLTAS_ENCODER_DA_CHAVE_PARA_CENTRO 730
+// Margem de erro da centralização
+#define OFFSET 5
+
+// Força máxima. Velocidade não chega a importar aqui, logo consideramos um valor mais baixo tempo para tenhamos maior precisão
+#define MAX_STRENGTH 160
+
+// Pulsos mínimo e máximo do servo utilizado (testado empiricamente)
 #define MIN_PULSE 1050
 #define MAX_PULSE 4700
 #define PULSE_RANGE MAX_PULSE-MIN_PULSE
+
+// Valores do contador de pulso: [-4500,4500] dá cerca de 2,5 voltas de um lado para o outro
 #define EDGE_COUNT 4500
 #define ENCODER_RANGE (2 * EDGE_COUNT)
+
+
+/*
+ * Variáveis para usar na EEPROM
+ */
+
+// Checagem simples para validar o número na EEPROM: se o nosso número mágico está guardado, então a distância guardada está correta
 #define MAGIC_NUMBER 0xA5A5
 #define EEPROM_MAGIC_ADDR 0
 #define EEPROM_DISTANCIA_ADDR 2
@@ -46,7 +64,7 @@ Rotary r = Rotary(ROTARY_ENC_A, ROTARY_ENC_B);
 
 
 void salvarDistancia(long valor) {
-    // Salva magic number (2 bytes)
+    // Salva número mágico (2 bytes)
     eeprom_update_word((uint16_t*)EEPROM_MAGIC_ADDR, MAGIC_NUMBER);
 
     // Salva o valor (4 bytes)
@@ -56,14 +74,15 @@ void salvarDistancia(long valor) {
 }
 
 bool carregarDistancia() {
-    uint16_t magic = eeprom_read_word((uint16_t*)EEPROM_MAGIC_ADDR);
-    if (magic != MAGIC_NUMBER) {
-        Serial.println("EEPROM vazia, usando distância padrão: 730");
+    // carrega o número mágico
+    // checamos se o número mágico é realmente o que colocamos previamente em algum momento
+    if ((uint16_t)eeprom_read_word((uint16_t*)EEPROM_MAGIC_ADDR) != MAGIC_NUMBER) {
+        Serial.println("EEPROM vazia/corrompida/estranha, usando distância padrão: 730");
         return false;
     }
-
+    // Chegando aqui, o número mágico está correto. Logo, a distância até a chave guardada também.
     distanciaChave = (long)eeprom_read_dword((uint32_t*)EEPROM_DISTANCIA_ADDR);
-    Serial.print("Distância carregada da EEPROM: ");
+    Serial.print("Distancia carregada da EEPROM: ");
     Serial.println(distanciaChave);
     return true;
 }
@@ -92,7 +111,7 @@ void encontrarCentro(){
       while(tempoParaFrenagem + 1000 > millis());
     };
     stop();
-    // enquanto nao estiver na chave, roda no sentido horario
+    // enquanto não estiver na chave, roda no sentido hoário
     while(absolute_sw) move(160, 1);
     stop();
     tempoParaFrenagem = millis();
@@ -115,47 +134,43 @@ void idle() {
 }
 
 void centralizarVolante() {
+    // Procura centralizar o volante com um erro de OFFSET
     while(count < centro-OFFSET) move(153, 1);
     stop();
     while(count > centro+OFFSET) move(153, 0);
     stop();
+    // Espera um pouco para ver se realmente estamos centralizados
     long long time = millis();
     while(time + 1000 > millis());
-
     if(count < centro + OFFSET || count > centro - OFFSET) centralizado = true;
     idle();
     count = 0;
 }
 
-void servo() {
-    OCR1A = map(count, -EDGE_COUNT, EDGE_COUNT, MAX_PULSE, MIN_PULSE);
-}
-
 void setup() {
+    // Inicia serial para debug
     Serial.begin(115200);
     r.begin(true);
+    // Configuração de portas (os mesmos do starter code exceto pelo botão e servo)
     PCICR |= (1 << ROTARY_ENC_PCINT_AB_IE);
     PCMSK2 |= (1 << ROTARY_ENC_PCINT_A) | (1 << ROTARY_ENC_PCINT_B);
 
-    TCCR1A = (1<<WGM11);
-    TCCR1B = (1<<WGM12) | (1<<WGM13) | (1<<CS11); // Prescaler 8
-    TCCR1A |= (1<<COM1A1); // Ativa saída PWM no OC1A (PB1/SERVO)
+    // Timer e pwm do servo
+    TCCR1A |= ((1<<WGM11) | (1<<COM1A1)); // Ativa saída PWM no OC1A (PB1/SERVO)
+    TCCR1B |= (1<<WGM12) | (1<<WGM13) | (1<<CS11); // Prescaler 8
     ICR1 = 39999; // 50 Hz (16MHz / 8 / 40000)
     OCR1A = (MAX_PULSE + MIN_PULSE) / 2; // Inicia no centro (90°)
 
-    DDRB &= ~((1<<GP_BUTTON)|(1<<POS_SENSOR));
-    DDRB |= (1<<ATUA_CW)|(1<<ATUA_CCW)|(1<<ATUA_STRENGTH)|(1<<SERVO);
+    DDRB &= ~((1<<GP_BUTTON) | (1<<POS_SENSOR));
+    DDRB |= ((1<<ATUA_CW) | (1<<ATUA_CCW) | (1<<ATUA_STRENGTH) | (1<<SERVO));
 
-    PORTB |= (1<<POS_SENSOR);
-    PORTB |= (1<<GP_BUTTON);
-    PORTB &= ~(1<<ATUA_CW);
-    PORTB &= ~(1<<ATUA_CCW);
+    PORTB |= ((1<<POS_SENSOR) | (1<<GP_BUTTON));
+    PORTB &= ~((1<<ATUA_CW)|(1<<ATUA_CCW));
 
     // PWM do volante
     OCR2A = 0;
-    TCCR2A = (1<<WGM20);
+    TCCR2A = ((1<<WGM20) | (1<<COM2A1));
     TCCR2B = (1<<CS21);
-    TCCR2A |= (1<<COM2A1);
 
     sei();
     idle();
@@ -166,25 +181,28 @@ void setup() {
 void loop() {
     gpLeitura = (PINB & (1<<GP_BUTTON)) ? 1 : 0;
     if(!centralizado){
+        // Continuamos a centralizar o volante enquanto ele não estiver assim
         centralizarVolante();
     } else {
         // Já está centralizado. Então podemos mexer no servo
-        servo();
+        // Aqui usamos a função map de forma inversa: min -> max e max -> min:
+        OCR1A = map(count, -EDGE_COUNT, EDGE_COUNT, MAX_PULSE, MIN_PULSE);
         // Botão de calibração
         if(!gpLeitura && lastGpLeitura && millis() - millisGp > DEBOUNCE) {
+            // Salvamos a distancia até a chave
             salvarDistancia(count+distanciaChave);
-            distanciaChave = distanciaChave + count;  // Atualiza a variável também
-            count = 0;  // Reseta porque agora está no novo centro
             millisGp = millis();
         }
     }
-    if (millis()%300==0) {
-        Serial.print(count);
-        Serial.print(", ");
-        Serial.println(absolute_sw==true?'1':'0');
-        Serial.println(centralizado);
-    }
     lastGpLeitura = gpLeitura;
+    // Informações de debug
+    if (millis()%300==0) {
+        Serial.print("Posicao do count: ");
+        Serial.print(count);
+        Serial.print("; Na chave? ");
+        Serial.print(absolute_sw==true?"sim":"nao");
+        Serial.println(centralizado==true?"; Centralizado? sim":"; Centralizado? nao");
+    }
 }
 
 ISR(PCINT2_vect) {
